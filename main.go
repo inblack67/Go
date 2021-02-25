f254418a739d651654c433ff2fc200bb96a6cbdd
Aman Bhardwaj <inblack>
2021-02-25 13:04:46 +0530

    pointers

--- main.go
+++ main.go
@@ -1,27 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
+import "fmt"
 
-func panicker () {
-	fmt.Println("about to panic")
-	defer func () {
-		if err := recover(); err != nil {
-			log.Println("recovery err = ", err)
-		}
-	}()
-	panic("packied!!")
+// Hello ...
+type Hello struct {
+	reply string
 }
 
-func main(){
-	// panic => exceptions
-	// fmt.Println("start")
-	// defer fmt.Println("end")	// panic happens after the deferred statements are exec 
-	// panic("panicking")
+func main () {
+	a := 12
+	b := a	// copy
+	c := &a
+	*c = 69
+	fmt.Println(a, b, *c)	// 69, 12, 69
 
-	fmt.Println("start")
-	panicker()
-	fmt.Println("end")
+
+	// go does not allow pointer arithmetic
+	arr := [3]int{1,2,3}
+	p1 := &arr[0]
+	p2 := &arr[1]
+	p3 := &arr[2]
+	fmt.Printf("p1 = %p, p2 = %p, p3 = %p\n", p1, p2, p3)
+	// sub := p3 - p1	// not allowed
+
+	// default val for pointers => nil
+
+	greet := new(Hello)
+	// (*greet).reply = "worlds"	// original way
+	greet.reply = "worlds"	// syntactic sugar
+	fmt.Println("greet.reply = ", greet.reply)	// worlds
+
+	// arrays are value types => maps and slices are not.
 }
--- pointers/index.go
+++ pointers/index.go
@@ -2,16 +2,33 @@ package main
 
 import "fmt"
 
-func main(){
-	a := 5
-	b := &a
+// Hello ...
+type Hello struct {
+	reply string
+}
 
-	fmt.Println(a,b)
-	fmt.Printf("%T\n", a)
-	fmt.Printf("%T\n", b) // *int
-	fmt.Printf("%d\n", *b) // 5
-	fmt.Printf("%d\n", *&a) // 5
+func main () {
+	a := 12
+	b := a	// copy
+	c := &a
+	*c = 69
+	fmt.Println(a, b, *c)	// 69, 12, 69
 
-	*b = 20
-	fmt.Println(a)	// 20
+
+	// go does not allow pointer arithmetic
+	arr := [3]int{1,2,3}
+	p1 := &arr[0]
+	p2 := &arr[1]
+	p3 := &arr[2]
+	fmt.Printf("p1 = %p, p2 = %p, p3 = %p\n", p1, p2, p3)
+	// sub := p3 - p1	// not allowed
+
+	// default val for pointers => nil
+
+	greet := new(Hello)
+	// (*greet).reply = "worlds"	// original way
+	greet.reply = "worlds"	// syntactic sugar
+	fmt.Println("greet.reply = ", greet.reply)	// worlds
+
+	// arrays are value types => maps and slices are not.
 }

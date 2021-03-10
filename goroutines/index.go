bf51a7c7fba0415f38d208d666ee36c7e44ecd41
Aman Bhardwaj <inblack>
2021-03-10 21:57:01 +0530

    go routines

---
+++ goroutines/index.go
@@ -0,0 +1,28 @@
+package myroutines
+
+import (
+	"fmt"
+	"time"
+)
+
+func greet(){
+	fmt.Println("hello worlds")
+}
+
+func main() {
+	go greet()
+
+	var msg = "hello"
+	go func ()  {
+		fmt.Println(msg)	// can access it because of closures but the output will be => bye => race condition
+	}()
+	msg = "bye" // this var will be updated first then our above go routing will be called => after the end of main func
+
+	var msg2 = "hello"
+	go func (msg2 string)  {
+		fmt.Println(msg2)	// hello
+	}(msg2)	// copy saved
+	msg2 = "bye"
+
+	time.Sleep(100 * time.Millisecond)
+}
--- main.go
+++ main.go
@@ -1,34 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-// Hello ...
-type Hello struct {
-	reply string
-}
+var wg = sync.WaitGroup{}
 
-func main () {
-	a := 12
-	b := a	// copy
-	c := &a
-	*c = 69
-	fmt.Println(a, b, *c)	// 69, 12, 69
-
-
-	// go does not allow pointer arithmetic
-	arr := [3]int{1,2,3}
-	p1 := &arr[0]
-	p2 := &arr[1]
-	p3 := &arr[2]
-	fmt.Printf("p1 = %p, p2 = %p, p3 = %p\n", p1, p2, p3)
-	// sub := p3 - p1	// not allowed
-
-	// default val for pointers => nil
-
-	greet := new(Hello)
-	// (*greet).reply = "worlds"	// original way
-	greet.reply = "worlds"	// syntactic sugar
-	fmt.Println("greet.reply = ", greet.reply)	// worlds
-
-	// arrays are value types => maps and slices are not.
+func main() {
+	var msg = "hello"
+	wg.Add(1)	// added one wait group
+	go func (msg string)  {
+		fmt.Println(msg)
+		wg.Done() // done with this group => decrement
+	}(msg)
+	msg = "bye"
+	wg.Wait()	// exit
 }

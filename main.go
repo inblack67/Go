2e89b04109c183bd58b66a5c1c103906cc75c5bc
ryuzaki <ryuzaki>
2021-05-30 19:33:29 +0530

    serving static files

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net/http"
+)
 
 func main (){
-	var name string
-	fmt.Println("what is your name")
-	fmt.Scanln(&name)
-	fmt.Println("hello", name)
-
+	port := flag.String("p", "5000", "port")
+	dir:= flag.String("d", "static", "dir")
+	flag.Parse()
+	http.Handle("/", http.FileServer(http.Dir(*dir)))
+	log.Printf("Serving %s on port %s \n", *dir, *port)
+	log.Fatal(http.ListenAndServe(":"+ *port, nil))
 }
